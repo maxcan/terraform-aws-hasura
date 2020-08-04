7bcfd4d05bdc78dc1c76872c430eb36b6174d253
Max Cantor tbmb13-desktop <max@cantor.mx>
2020-08-04 15:25:10 -0700

    use new domain validation options syntax

--- main.tf
+++ main.tf
@@ -29,12 +29,24 @@ data "aws_route53_zone" "hasura" {
 }
 
 resource "aws_route53_record" "hasura_validation" {
+
+
+  for_each = {
+    for dvo in aws_acm_certificate.hasura.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
   depends_on = [aws_acm_certificate.hasura]
-  name       = aws_acm_certificate.hasura.domain_validation_options[0]["resource_record_name"]
-  type       = aws_acm_certificate.hasura.domain_validation_options[0]["resource_record_type"]
-  zone_id    = data.aws_route53_zone.hasura.zone_id
-  records    = [aws_acm_certificate.hasura.domain_validation_options[0]["resource_record_value"]]
-  ttl        = 300
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.hasura.zone_id
+
 }
 
 resource "aws_acm_certificate_validation" "hasura" {
